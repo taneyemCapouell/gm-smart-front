6ec9f03f9a86d004818635a5025036f0d3ad87f6
taneyemCapouell <109171805+taneyemCapouell@users.noreply.github.com>
2024-06-28 17:26:26 +0200

    commint link
    change link of backend

--- src/utils/axios-custum.ts
+++ src/utils/axios-custum.ts
@@ -1,18 +1,18 @@
 import axios from "axios";
 
 export default axios.create({
-  // baseURL: "http://localhost:8000/api/v1/",
-  baseURL : 'https://loving-vaughan.212-227-211-20.plesk.page/api-gm-smart/public/api/v1/'
+//   baseURL: "http://localhost:8000/api/v1/",
+  baseURL : 'https://eloquent-bohr.212-227-211-20.plesk.page/public/api/v1/'
 });
 
 export const http_client = (token: string) =>
   axios.create({
     // baseURL: "http://localhost:8000/api/v1/",
-    baseURL : 'https://loving-vaughan.212-227-211-20.plesk.page/api-gm-smart/public/api/v1/',
+    baseURL : 'https://eloquent-bohr.212-227-211-20.plesk.page/public/api/v1/',
     headers: {
       Authorization: `Bearer ${token}`,
     },
   });
 
-// export const baseURL = `http://localhost:8000`;
-export const baseURL = `https://loving-vaughan.212-227-211-20.plesk.page/api-gm-smart/public/`
+// export const baseURL = "http://localhost:8000"
+export const baseURL = "https://eloquent-bohr.212-227-211-20.plesk.page/public"
